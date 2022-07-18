7a4f54d4ae3098a99f8e4a222e37950edd860038
sogandtatlari <sogand.tatlarii@yahoo.com>
2022-07-18 21:23:12 +0430

    search

--- components/searchpage/user/UserProfile.js
+++ components/searchpage/user/UserProfile.js
@@ -1,16 +1,13 @@
 import Image from "next/image";
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Divider from "@mui/material/Divider";
-import {Stack} from "@mui/material";
+
 
 
 const UserProfile = ({name, date, username, imageURL}) => {
     return (
         <>
             <div
-                className=" flex flex-col md:flex-row w-full   items-center space-y-6 space-x-4  md:justify-evenly md:space-x-6">
+                className=" flex flex-col md:flex-row w-full   items-center space-y-6  md:justify-evenly md:space-x-6">
                 {imageURL ? (
                     <div className=" w-[120px] h-[120px] md:ml-8   dark:ring-[#053bff] rounded-full ">
                         <Image
@@ -28,7 +25,7 @@ const UserProfile = ({name, date, username, imageURL}) => {
                     )
                 }
                 <div
-                    className="flex items-center w-full justify-around space-x-6 px-2  md:flex-1 md:items-start md:justify-between">
+                    className="flex items-center w-full justify-around space-x-6 px-2 text-center md:text-left md:flex-1 md:items-start md:justify-between">
                     <h2 className="w-32 md:w-44 font-mono text-lg font-bold text-gray-400 dark:text-gray-50 md:text-2xl">
                         {name}{' '}
                         <span className="inline-block font-mono text-sm text-blue-400">
